f414702e9aee231854b1d749adc9da5d0cfd9a55
Rafiariyanta <rafiariyanta08@gmail.com>
2026-06-24 09:10:53 +0700

    feat: re config

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,13 @@
 /// <reference types="vite/client" />
 /// <reference types="react-router" />
-/// <reference types="@shopify/oxygen-workers-types" />
+/// //<reference types="@shopify/oxygen-workers-types" />
 /// <reference types="@shopify/hydrogen/react-router-types" />
+
+declare global {
+  interface ExecutionContext {
+    waitUntil(promise: Promise<unknown>): void;
+  }
+}
 
 // Enhance TypeScript's built-in typings.
 import '@total-typescript/ts-reset';
